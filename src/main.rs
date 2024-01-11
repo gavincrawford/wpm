b3911a9b91983be73c49f07fb4eab8acc60176e6
gavincrawford <94875769+gavincrawford@users.noreply.github.com>
2024-01-11 10:23:42 -0700

    Raw terminal setup, key handler

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,66 @@
+use std::{
+    io::{stdout, Stdout, Write},
+    time::Duration,
+};
+
+use crossterm::{
+    cursor::MoveTo,
+    event::{poll, read, Event, KeyCode},
+    execute, queue,
+    style::{Print, Stylize},
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
+};
 use rand::seq::SliceRandom;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
+    // get phrase from wordlist
     const ENG_1K: &str = include_str!("../wordlist/eng_1k.txt");
     let tokens: Vec<&str> = str_to_tokens(ENG_1K);
     let phrase = tokens_to_phrase(25, &tokens);
-    dbg!(phrase);
+
+    // basic terminal renderer
+    let mut stdout = stdout();
+    enable_raw_mode().expect("failed to enable raw mode");
+    clear(&mut stdout);
+    loop {
+        // render
+        clear(&mut stdout);
+        for word in phrase.split_whitespace() {
+            queue!(stdout, Print(word.black().on_grey()), Print(" "))?;
+        }
+        queue!(stdout, MoveTo(0, 0))?;
+        stdout.flush()?;
+
+        // handle events
+        use Event::*;
+        use KeyCode::*;
+        if !poll(Duration::from_secs(1))? {
+            continue;
+        }
+        match read()? {
+            Key(key) => match key.code {
+                Esc => break,
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+    disable_raw_mode().expect("failed to disable raw mode");
+    clear(&mut stdout);
+
+    // done
+    Ok(())
+}
+
+/// Clear the screen via the given `stdout` handle.
+fn clear(io: &mut Stdout) {
+    execute!(
+        io,
+        MoveTo(0, 0),
+        Clear(ClearType::All),
+        Clear(ClearType::Purge)
+    )
+    .expect("failed to clear screen")
 }
 
 /// Split a string into a vector of its lines.
