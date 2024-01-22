3de0c8d841ad242b59a7e40173385123b2242f80
gavincrawford <94875769+gavincrawford@users.noreply.github.com>
2024-01-22 10:18:20 -0700

    Refactor rendering code
    Also switched to using an events system instead of simple booleans to keep track of what we need to render. Cleaner and requires less code.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
-use std::{
-    io::{stdout, Stdout, Write},
-    time::{Duration, Instant},
-};
-
 use clap::{arg, Command};
-use crossterm::{
-    cursor::MoveTo,
-    event::{poll, read, Event, KeyCode},
-    execute, queue,
-    style::{Print, Stylize},
-    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType},
-};
 use tokens::{str_to_tokens, tokens_to_phrase, ENG_10K, ENG_1K};
 
 mod tokens;
+mod type_render;
 
 fn main() -> Result<(), std::io::Error> {
     // get args
@@ -36,117 +25,6 @@ fn main() -> Result<(), std::io::Error> {
     let tokens: Vec<&str> = str_to_tokens(wordlist);
     let phrase = tokens_to_phrase(10, &tokens);
 
-    // set up variables for the renderer
-    let mut stdout = stdout(); // stdout handle
-    let mut n_miss: usize = 0; // miss counter (error count)
-    let mut miss: bool = false; // miss render flag
-    let mut n_hit: usize = 0; // hit counter (cursor pos)
-    let mut hit: bool = false; // hit render flag
-    let timer = Instant::now(); // timer for WPM calculation
-    enable_raw_mode().expect("failed to enable raw mode");
-    clear(&mut stdout);
-
-    // render base text
-    queue!(stdout, MoveTo(0, 0))?;
-    for c in phrase.chars() {
-        if c == ' ' {
-            queue!(stdout, Print('_'.dark_grey().on_grey()))?;
-        } else {
-            queue!(stdout, Print(c.black().on_grey()))?;
-        }
-    }
-
-    // play loop
-    loop {
-        // render
-        let size = size().expect("Failed to read screen size.");
-        if hit {
-            queue!(
-                stdout,
-                move_to_wrap(n_hit - 1, size),
-                Print((phrase.as_bytes()[n_hit - 1] as char).black().on_green())
-            )?;
-            hit = false;
-        } else if miss {
-            queue!(
-                stdout,
-                move_to_wrap(n_hit, size),
-                Print((phrase.as_bytes()[n_hit] as char).on_red())
-            )?;
-            miss = false;
-        }
-        queue!(stdout, move_to_wrap(n_hit, size))?;
-        stdout.flush()?;
-
-        // end condition
-        if n_hit == phrase.len() {
-            break;
-        }
-
-        // handle events
-        use Event::*;
-        use KeyCode::*;
-        if !poll(Duration::from_millis(200))? {
-            continue;
-        }
-        match read()? {
-            Key(key) => match key.code {
-                Esc => break,
-                Backspace => n_hit -= 1,
-                Char(char) => {
-                    let next = phrase.chars().nth(n_hit);
-                    if char == next.unwrap_or('~') {
-                        n_hit += 1;
-                        hit = true;
-                    } else {
-                        n_miss += 1;
-                        miss = true;
-                    }
-                }
-                _ => {}
-            },
-            _ => {}
-        }
-    }
-    disable_raw_mode().expect("failed to disable raw mode");
-    clear(&mut stdout);
-
-    // give user wpm
-    println!(
-        "GROSS: {:.2}wpm\nNET:   {:.2}wpm",
-        wpm_gross(phrase.len(), timer.elapsed()),
-        wpm_net(phrase.len(), n_miss, timer.elapsed())
-    );
-
-    // done
-    Ok(())
-}
-
-/// Clear the screen via the given `stdout` handle.
-fn clear(io: &mut Stdout) {
-    execute!(
-        io,
-        MoveTo(0, 0),
-        Clear(ClearType::All),
-        Clear(ClearType::Purge)
-    )
-    .expect("failed to clear screen")
-}
-
-/// Move to position by char, with wrap in respect to `size`.
-fn move_to_wrap(pos: usize, size: (u16, u16)) -> MoveTo {
-    MoveTo(
-        (pos % size.0 as usize) as u16,
-        (pos / size.0 as usize) as u16,
-    )
-}
-
-/// Calculate raw WPM from typed characters and time.
-fn wpm_gross(k: usize, dur: Duration) -> f32 {
-    (k as f32 / 5.) / (dur.as_secs() as f32 / 60.)
-}
-
-/// Calculate net WPM from typed characters and time, with consideration for errors.
-fn wpm_net(k: usize, e: usize, dur: Duration) -> f32 {
-    wpm_gross(k, dur) - (e as f32 / (dur.as_secs() as f32 / 60.))
+    // render type
+    type_render::TypeRenderer::new(phrase).render()
 }
---
+++ src/type_render.rs
@@ -0,0 +1,164 @@
+use std::{
+    io::{stdout, Stdout, Write},
+    time::{Duration, Instant},
+};
+
+use crossterm::{
+    cursor::MoveTo,
+    event::{poll, read, Event, KeyCode},
+    execute, queue,
+    style::{Print, Stylize},
+    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType},
+};
+
+/// Renders a type with a given phrase. A type refers to one 'test' of typing ability on the given
+/// phrase, timing the response and evaluating the demonstrated WPM.
+pub struct TypeRenderer {
+    /// Phrase the user will be tested on.
+    phrase: String,
+    /// Tracks events.
+    events: Vec<TypeEvent>,
+}
+
+/// Represents all types of events that could occur during a type test.
+enum TypeEvent {
+    Miss,
+    Hit,
+}
+
+impl TypeRenderer {
+    pub fn new(phrase: String) -> Self {
+        Self {
+            phrase,
+            events: vec![],
+        }
+    }
+
+    pub fn render(&mut self) -> Result<(), std::io::Error> {
+        // set up variables for the renderer
+        let mut stdout = stdout(); // stdout handle
+        let mut n_miss: usize = 0; // miss counter (error count)
+        let mut n_hit: usize = 0; // hit counter (cursor pos)
+        let timer = Instant::now(); // timer for WPM calculation
+        enable_raw_mode().expect("failed to enable raw mode");
+        clear(&mut stdout);
+
+        // render base text
+        queue!(stdout, MoveTo(0, 0))?;
+        for c in self.phrase.chars() {
+            if c == ' ' {
+                queue!(stdout, Print('_'.dark_grey().on_grey()))?;
+            } else {
+                queue!(stdout, Print(c.black().on_grey()))?;
+            }
+        }
+
+        // play loop
+        loop {
+            // render
+            let size = size().expect("Failed to read screen size.");
+            self.events.retain(|event| {
+                use TypeEvent::*;
+                match event {
+                    Miss => {
+                        queue!(
+                            stdout,
+                            move_to_wrap(n_hit, size),
+                            Print((self.phrase.as_bytes()[n_hit] as char).on_red())
+                        )
+                        .unwrap();
+                    }
+                    Hit => {
+                        queue!(
+                            stdout,
+                            move_to_wrap(n_hit - 1, size),
+                            Print(
+                                (self.phrase.as_bytes()[n_hit - 1] as char)
+                                    .black()
+                                    .on_green()
+                            )
+                        )
+                        .unwrap();
+                    }
+                }
+
+                // remove event
+                false
+            });
+            queue!(stdout, move_to_wrap(n_hit, size))?;
+            stdout.flush()?;
+
+            // end condition
+            if n_hit == self.phrase.len() {
+                break;
+            }
+
+            // handle events
+            use Event::*;
+            use KeyCode::*;
+            if !poll(Duration::from_secs(1))? {
+                continue;
+            } else {
+                match read()? {
+                    Key(key) => match key.code {
+                        Esc => break,
+                        Backspace => n_hit -= 1,
+                        Char(char) => {
+                            let next = self.phrase.chars().nth(n_hit);
+                            if char == next.unwrap_or('~') {
+                                n_hit += 1;
+                                self.events.push(TypeEvent::Hit);
+                            } else {
+                                n_miss += 1;
+                                self.events.push(TypeEvent::Miss);
+                            }
+                        }
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            }
+        }
+        disable_raw_mode().expect("failed to disable raw mode");
+        clear(&mut stdout);
+
+        // give user wpm
+        println!(
+            "GROSS: {:.2}wpm\nNET:   {:.2}wpm",
+            wpm_gross(self.phrase.len(), timer.elapsed()),
+            wpm_net(self.phrase.len(), n_miss, timer.elapsed())
+        );
+
+        // done
+        Ok(())
+    }
+}
+
+/// Clear the screen via the given `stdout` handle.
+fn clear(io: &mut Stdout) {
+    execute!(
+        io,
+        MoveTo(0, 0),
+        Clear(ClearType::All),
+        Clear(ClearType::Purge)
+    )
+    .expect("failed to clear screen")
+}
+
+/// Move to position by char, with wrap in respect to `size`.
+fn move_to_wrap(pos: usize, size: (u16, u16)) -> MoveTo {
+    MoveTo(
+        (pos % size.0 as usize) as u16,
+        (pos / size.0 as usize) as u16,
+    )
+}
+
+/// Calculate raw WPM from typed characters and time.
+fn wpm_gross(k: usize, dur: Duration) -> f32 {
+    (k as f32 / 5.) / (dur.as_secs() as f32 / 60.)
+}
+
+/// Calculate net WPM from typed characters and time, with consideration for errors.
+fn wpm_net(k: usize, e: usize, dur: Duration) -> f32 {
+    wpm_gross(k, dur) - (e as f32 / (dur.as_secs() as f32 / 60.))
+}
