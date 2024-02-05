719534085af15a14c608a3e157e44339fc2b5439
gavincrawford <94875769+gavincrawford@users.noreply.github.com>
2024-02-05 10:11:42 -0700

    Menu cursor

--- src/render/menu.rs
+++ src/render/menu.rs
@@ -1,8 +1,11 @@
-use std::{io::stdout, time::Duration};
+use std::{
+    io::{stdout, Write},
+    time::Duration,
+};
 
 use crossterm::{
-    cursor::{Hide, MoveTo, MoveToNextLine, Show},
-    event::{poll, read, Event, KeyCode},
+    cursor::{Hide, MoveRight, MoveTo, MoveToNextLine, Show},
+    event::{poll, read, Event, KeyCode, KeyEvent},
     execute, queue,
     style::{Print, Stylize},
 };
@@ -10,31 +13,42 @@ use crossterm::{
 use super::util::clear;
 
 /// Renders the menu.
-pub struct MenuRenderer;
+pub struct MenuRenderer {
+    /// Selected menu option, where 0 is the top.
+    cursor: usize,
+}
 
 impl MenuRenderer {
     pub fn new() -> Self {
-        Self {}
+        Self { cursor: 0 }
     }
 
     pub fn render(&mut self) -> Result<(), std::io::Error> {
         let mut stdout = stdout();
-        clear(&mut stdout);
         loop {
             // render menu elements
-            queue!(stdout, Hide, MoveTo(0, 0), Print("WPM".on_grey().grey()))?;
-            execute!(
+            clear(&mut stdout);
+            const ELEMENTS: [&str; 4] = ["easy 10", "hard 10", "custom", "stats"];
+            queue!(
                 stdout,
                 Hide,
                 MoveTo(0, 0),
-                Print("WPM"),
-                MoveToNextLine(1),
-                Print(" easy"),
-                MoveToNextLine(1),
-                Print(" hard"),
-                MoveToNextLine(1),
-                Print(" profile")
+                Print("WPM".on_dark_grey().grey()),
+                MoveToNextLine(1)
             )?;
+            for (idx, element) in ELEMENTS.iter().enumerate() {
+                if idx == self.cursor {
+                    queue!(
+                        stdout,
+                        MoveRight(3),
+                        Print(element.black().on_grey()),
+                        MoveToNextLine(1)
+                    )?;
+                } else {
+                    queue!(stdout, MoveRight(2), Print(element), MoveToNextLine(1))?;
+                }
+            }
+            stdout.flush()?;
 
             // handle events
             if !poll(Duration::from_millis(1000))? {
@@ -45,17 +59,35 @@ impl MenuRenderer {
                 match read()? {
                     Key(key) => match key.code {
                         Esc => break,
-                        _ => {
-                            // TODO handle keys
-                            break;
-                        }
+                        _ => self.handle_key(key),
                     },
                     _ => {}
                 }
             }
+
+            // clamp cursor after handling events, just in case it went out of bounds
+            self.cursor = self.cursor.clamp(0, ELEMENTS.len() - 1);
         }
         clear(&mut stdout);
         execute!(stdout, Show)?;
         Ok(())
+    }
+
+    /// Handles a keypress.
+    fn handle_key(&mut self, key: KeyEvent) {
+        use KeyCode::*;
+        match key.code {
+            Down | Char('j') => {
+                if let Some(i) = self.cursor.checked_add(1) {
+                    self.cursor = i;
+                }
+            }
+            Up | Char('k') => {
+                if let Some(i) = self.cursor.checked_sub(1) {
+                    self.cursor = i;
+                }
+            }
+            _ => {}
+        }
     }
 }
