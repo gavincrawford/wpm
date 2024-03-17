3ee2d01f03e24a4034c5f8e18a9d535b2fdeea7c
gavincrawford <94875769+gavincrawford@users.noreply.github.com>
2024-03-17 08:11:50 -0600

    Working submenu prototype

--- src/render/menu.rs
+++ src/render/menu.rs
@@ -6,32 +6,72 @@ use std::{
 use super::{test::*, util::*, wordlist::*};
 use crate::{profile::Profile, render::profile::ProfileRenderer};
 use crossterm::{
-    cursor::{Hide, MoveRight, MoveTo, MoveToNextLine, Show},
+    cursor::{Hide, MoveRight, MoveTo, MoveToNextLine, MoveUp, Show},
     event::{poll, read, Event, KeyCode, KeyEvent},
     execute, queue,
     style::{Print, Stylize},
 };
 
+/// Gap between menu stacks.
+const GAP: usize = 1;
+
+/// Margin between menu and screen edge.
+const MARGIN: usize = 3;
+
 /// Renders the menu.
 pub struct MenuRenderer {
-    /// Selected menu option, where 0 is the top.
-    cursor: usize,
+    /// Selected menu option for each menu currently open.
+    cursor: Vec<usize>,
     /// Active profile.
     profile: Option<Profile>,
     /// Profile path. If not overridden, it will default to "profile".
     profile_path: String,
-    /// Menu elements.
-    menu: Vec<(String, MenuElement)>,
-    /// Active menu. Only applies to menu elements that are rendered in this structure.
-    active_menu: Option<MenuElement>,
+    /// Root menu element.
+    root_menu: MenuElement,
 }
 
-/// Represents menu options when rendered.
+/// Represents menu options and submenus.
+struct MenuElement {
+    label: String,
+    subitems: Option<Vec<MenuElement>>,
+    action: MenuAction,
+}
+
+impl MenuElement {
+    /// Creates a `MenuElement` that does *not* utilize an action, and represents a submenu.
+    pub fn new_menu(label: impl Into<String>, subitems: Vec<MenuElement>) -> Self {
+        Self {
+            label: label.into(),
+            subitems: Some(subitems),
+            action: MenuAction::None,
+        }
+    }
+
+    /// Creates a `MenuElement` that utilizes an action.
+    pub fn new_action(label: impl Into<String>, action: MenuAction) -> Self {
+        Self {
+            label: label.into(),
+            subitems: None,
+            action,
+        }
+    }
+
+    /// Get an immutable reference to the subitems of this element.
+    pub fn subitems(&self) -> Option<&Vec<MenuElement>> {
+        if let Some(elements) = &self.subitems {
+            Some(elements)
+        } else {
+            None
+        }
+    }
+}
+
+/// Represents menu actions, like starting tests or viewing the profile.
 #[derive(Clone)]
-enum MenuElement {
+enum MenuAction {
     Test { wordlist: Wordlist, mode: Mode },
     Profile,
-    Settings,
+    None,
 }
 
 impl MenuRenderer {
@@ -53,34 +93,39 @@ impl MenuRenderer {
         let profile_path = profile_path.unwrap_or(String::from("profile"));
 
         // make menu items
-        let menu = vec![
-            (
-                "10 words easy",
-                MenuElement::Test {
-                    wordlist: Wordlist::English1k,
-                    mode: Mode::Words(10),
-                },
-            ),
-            (
-                "15 seconds easy",
-                MenuElement::Test {
-                    wordlist: Wordlist::English10k,
-                    mode: Mode::Time(Duration::from_secs(15)),
-                },
-            ),
-            ("profile statistics", MenuElement::Profile),
-            ("settings", MenuElement::Settings),
-        ];
-        let menu = menu
-            .iter()
-            .map(|e| (String::from(e.0), e.1.clone()))
-            .collect::<Vec<(String, MenuElement)>>();
         Self {
-            cursor: 0,
+            cursor: vec![0],
             profile,
             profile_path,
-            menu,
-            active_menu: None,
+            root_menu: MenuElement::new_menu(
+                "root",
+                vec![
+                    MenuElement::new_action(
+                        "words 10",
+                        MenuAction::Test {
+                            wordlist: Wordlist::English1k,
+                            mode: Mode::Words(10),
+                        },
+                    ),
+                    MenuElement::new_action(
+                        "time 10",
+                        MenuAction::Test {
+                            wordlist: Wordlist::English1k,
+                            mode: Mode::Time(Duration::from_secs(10)),
+                        },
+                    ),
+                    MenuElement::new_action("profile", MenuAction::Profile),
+                    // TODO put settings here!!
+                    MenuElement::new_menu(
+                        "test menu",
+                        vec![
+                            MenuElement::new_action("xyz", MenuAction::None),
+                            MenuElement::new_action("xyz", MenuAction::None),
+                            MenuElement::new_action("xyz", MenuAction::None),
+                        ],
+                    ),
+                ],
+            ),
         }
     }
 
@@ -122,28 +167,54 @@ impl MenuRenderer {
             }
             queue!(stdout, MoveToNextLine(1))?;
 
-            // render menu elements, based on active menu
-            if let Some(menu) = &self.active_menu {
-                // other menus
-                match menu {
-                    MenuElement::Settings => {
-                        queue!(stdout, Print("SETTINGS TODO"))?; // TODO
+            // render main menu stack
+            let menus = self.get_menus_from_cursor();
+            let mut this_max_x: usize = 0;
+            let mut last_max_x: usize = 0;
+            for (depth, menu) in menus.iter().enumerate() {
+                // for each menu in the stack, print subitems
+                let mut lns = 0;
+                if let Some(elements) = menu.subitems() {
+                    for (idx, element) in elements.iter().enumerate() {
+                        // update max_x for use later
+                        if element.label.len() > this_max_x {
+                            this_max_x = element.label.len();
+                        }
+
+                        // display each line
+                        if depth == menus.len() - 1 {
+                            if idx == *self.cursor.last().expect("cursor is null") {
+                                queue!(
+                                    stdout,
+                                    MoveRight(MARGIN as u16 + 1 + last_max_x as u16),
+                                    Print(element.label.clone().black().on_grey()),
+                                    MoveToNextLine(1)
+                                )?;
+                            } else {
+                                queue!(
+                                    stdout,
+                                    MoveRight(MARGIN as u16 + last_max_x as u16),
+                                    Print(element.label.clone()),
+                                    MoveToNextLine(1)
+                                )?;
+                            }
+                        } else {
+                            queue!(
+                                stdout,
+                                MoveRight(MARGIN as u16 + last_max_x as u16),
+                                Print(element.label.clone().dark_grey()),
+                                MoveToNextLine(1)
+                            )?;
+                        }
+                        lns += 1;
                     }
-                    _ => {}
                 }
-            } else {
-                // main menu
-                for (idx, (label, _)) in self.menu.iter().enumerate() {
-                    if idx == self.cursor {
-                        queue!(
-                            stdout,
-                            MoveRight(3),
-                            Print(label.clone().black().on_grey()),
-                            MoveToNextLine(1)
-                        )?;
-                    } else {
-                        queue!(stdout, MoveRight(2), Print(label), MoveToNextLine(1))?;
-                    }
+                last_max_x = last_max_x + this_max_x + GAP;
+                this_max_x = 0;
+
+                // if this isn't the last menu, shift the cursor back up
+                if depth != menus.len() - 1 {
+                    queue!(stdout, MoveUp(lns))?;
                 }
             }
 
@@ -193,8 +264,8 @@ impl MenuRenderer {
                         Esc => {
                             // if the user is in a menu, leave that first
                             // otherwise, just exit the program itself
-                            if self.active_menu.is_some() {
-                                self.active_menu = None;
+                            if self.cursor.len() > 1 {
+                                self.cursor.pop();
                             } else {
                                 if let Some(profile) = &self.profile {
                                     profile
@@ -209,10 +280,8 @@ impl MenuRenderer {
                     },
                     _ => Ok(()),
                 };
+                // TODO clamp the cursor somewhere
             }
-
-            // clamp cursor after handling events, just in case it went out of bounds
-            self.cursor = self.cursor.clamp(0, self.menu.len() - 1);
         }
         clear(&mut stdout);
         execute!(stdout, Show)?;
@@ -224,19 +293,26 @@ impl MenuRenderer {
         use KeyCode::*;
         match key.code {
             Down | Char('j') => {
-                if let Some(i) = self.cursor.checked_add(1) {
-                    self.cursor = i;
+                if let Some(i) = self.cursor.last().expect("cursor is null").checked_add(1) {
+                    *self.cursor.last_mut().unwrap() = i;
                 }
             }
             Up | Char('k') => {
-                if let Some(i) = self.cursor.checked_sub(1) {
-                    self.cursor = i;
+                if let Some(i) = self.cursor.last().expect("cursor is null").checked_sub(1) {
+                    *self.cursor.last_mut().unwrap() = i;
                 }
             }
             Enter => {
-                if let Some(e) = self.menu.get(self.cursor) {
-                    use MenuElement::*;
-                    match &e.1 {
+                if let Some(e) = self
+                    .get_menus_from_cursor()
+                    .last()
+                    .unwrap()
+                    .subitems()
+                    .unwrap()
+                    .get(*self.cursor.last().unwrap())
+                {
+                    use MenuAction::*;
+                    match &e.action {
                         Test { mode, wordlist } => {
                             // get test wordlist information for later
                             let content = get_wordlist_content(wordlist);
@@ -265,8 +341,11 @@ impl MenuRenderer {
                                 ProfileRenderer::new(&profile).render()?
                             }
                         }
-                        Settings => {
-                            self.active_menu = Some(MenuElement::Settings);
+                        _ => {
+                            // if this item is a subitem, open it by pushing a new cursor
+                            if e.subitems.is_some() {
+                                self.cursor.push(0);
+                            }
                         }
                     }
                 }
@@ -274,5 +353,29 @@ impl MenuRenderer {
             _ => {}
         }
         Ok(())
+    }
+
+    /// Get menus from cursor position.
+    fn get_menus_from_cursor(&self) -> Vec<&MenuElement> {
+        // create vec and add the first menu, the root one
+        let mut menus = vec![];
+        menus.push(&self.root_menu);
+
+        // get menus down depth levels
+        for idx in 1..self.cursor.len() {
+            let cursor = self.cursor.get(idx - 1).unwrap();
+            menus.push(
+                menus
+                    .last()
+                    .expect("no last element")
+                    .subitems()
+                    .expect("no subitems")
+                    .get(*cursor)
+                    .expect("no item at cursor"),
+            );
+        }
+
+        // done
+        menus
     }
 }
