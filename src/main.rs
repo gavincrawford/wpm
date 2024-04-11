69ca1ae4bd3e47e6e2d0fe1c6d4667f098ed9392
gavincrawford <94875769+gavincrawford@users.noreply.github.com>
2024-04-11 13:33:16 -0600

    Add panic handler to prevent terminal artifacts
    Without panic handler, raw mode wouldn't get disabled when a panic occurs, causing the terminal to be basically unusable. This fixes that issue by showing the cursor and disabling raw mode under any circumstance, including during a panic.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::{io::stdout, panic::PanicInfo};
+
 use clap::{arg, Command};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::{
+    cursor::Show,
+    queue,
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
 
 mod profile;
 mod render;
@@ -10,6 +16,9 @@ fn main() -> Result<(), std::io::Error> {
         .arg(arg!(--profile <PATH> "Runs the app with the specified profile"))
         .arg(arg!(--"no-profile" "Runs the app without a profile to save to"))
         .get_matches();
+
+    // set panic hook in case anything goes wrong
+    std::panic::set_hook(Box::new(panic_handler));
 
     // enable raw terminal
     enable_raw_mode().expect("failed to enable raw mode");
@@ -28,4 +37,13 @@ fn main() -> Result<(), std::io::Error> {
 
     // done
     Ok(())
+}
+
+/// Handles any panics that may occur. This handler ensures that, if not already, the terminal is
+/// not in raw mode and the cursor is shown, so that the terminal environment does not get
+/// disrupted by a panic within the program.
+fn panic_handler(info: &PanicInfo) {
+    println!("{}", info);
+    queue!(stdout(), Show).unwrap();
+    disable_raw_mode().unwrap();
 }
