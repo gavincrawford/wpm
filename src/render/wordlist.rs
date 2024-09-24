f6246a12374d9ede90885513852f14fc08c4f74b
gavincrawford <94875769+gavincrawford@users.noreply.github.com>
2024-09-24 10:50:52 -0600

    Compress wordlists

--- src/render/wordlist.rs
+++ src/render/wordlist.rs
@@ -1,17 +1,19 @@
+use flate2::bufread::GzDecoder;
 use serde_derive::{Deserialize, Serialize};
+use std::io::Read;
 use strum_macros::{EnumIter, EnumString};
 
 /// English 1k most used
-pub const ENG_1K: &str = include_str!("../../wordlist/eng_1k.txt");
+pub const ENG_1K: &[u8] = include_bytes!("../../wordlist/eng_1k.txt.gz");
 
 /// English 5k most used
-pub const ENG_5K: &str = include_str!("../../wordlist/eng_5k.txt");
+pub const ENG_5K: &[u8] = include_bytes!("../../wordlist/eng_5k.txt.gz");
 
 /// English 10k most used
-pub const ENG_10K: &str = include_str!("../../wordlist/eng_10k.txt");
+pub const ENG_10K: &[u8] = include_bytes!("../../wordlist/eng_10k.txt.gz");
 
 /// English most commonly misspelled words
-pub const ENG_COMMON_MISSPELLED: &str = include_str!("../../wordlist/eng_misspelled.txt");
+pub const ENG_COMMON_MISSPELLED: &[u8] = include_bytes!("../../wordlist/eng_misspelled.txt.gz");
 
 /// Wordlist enumerator, which represents wordlists without carrying around all the weight.
 #[derive(Clone, Debug, Serialize, Deserialize, EnumIter, EnumString)]
@@ -25,10 +27,15 @@ pub enum Wordlist {
 /// Converts enum to wordlist content.
 pub fn get_wordlist_content(wordlist: &Wordlist) -> String {
     use super::wordlist::*;
-    match wordlist {
-        Wordlist::English1k => ENG_1K.into(),
-        Wordlist::English5k => ENG_5K.into(),
-        Wordlist::English10k => ENG_10K.into(),
-        Wordlist::EnglishCommonMisspelled => ENG_COMMON_MISSPELLED.into(),
-    }
+    let mut decoder = match wordlist {
+        Wordlist::English1k => GzDecoder::new(ENG_1K),
+        Wordlist::English5k => GzDecoder::new(ENG_5K),
+        Wordlist::English10k => GzDecoder::new(ENG_10K),
+        Wordlist::EnglishCommonMisspelled => GzDecoder::new(ENG_COMMON_MISSPELLED),
+    };
+    let mut buf = String::new();
+    decoder
+        .read_to_string(&mut buf)
+        .expect("Failed to decompress requested wordlist.");
+    buf
 }
