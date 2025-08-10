8a78b817e728035c273caca5f8beda84c9ac3182
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-08-10 00:47:56 -0600

    test: Test `LiveWPM` module

--- src/render/test/live_wpm.rs
+++ src/render/test/live_wpm.rs
@@ -58,3 +58,36 @@ impl LiveWPM {
         self.last_wpm
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread::sleep;
+
+    #[test]
+    fn keypresses_register() {
+        // create tracker and press once
+        let mut tracker = LiveWPM::new();
+        tracker.press();
+
+        // assert that the press was added to internal `keypresses` buffer
+        assert!(tracker.keypresses.len() > 0);
+    }
+
+    #[test]
+    fn average_wpm_is_correct() {
+        // find expected wpm with given keys/sec
+        const KEYPRESSES_PER_SEC: usize = 10;
+        let expected_wpm = wpm_gross(KEYPRESSES_PER_SEC - 1, WINDOW);
+
+        // create tracker and press keys over provided time period
+        let mut tracker = LiveWPM::new();
+        for _ in 0..KEYPRESSES_PER_SEC {
+            tracker.press();
+            sleep(WINDOW / KEYPRESSES_PER_SEC as u32);
+        }
+
+        // assert that the difference between expected and calculated is none
+        assert!((expected_wpm as i32 - tracker.wpm() as i32) == 0);
+    }
+}
